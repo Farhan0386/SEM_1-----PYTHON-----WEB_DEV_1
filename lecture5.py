79f0738aa39ce8c8c03d73a96a053f4d18c77378
Farhan Hussain <farhan03860@gmail.com>
2025-09-22 06:53:24 +0530

    .

---
+++ lecture5.py
@@ -0,0 +1,23 @@
+x=5   #variable assignment
+print(x)
+
+x=10  #reassignment
+print(x)
+
+#rules for giving name to variables
+#1.  can only contain a letter or underscore and numbers
+#2.  cannot start with a number
+#3.  cannot be a keyword
+#4.  case sensitive
+
+b="hello"
+print(b)
+print(type(b))
+
+c=3.5
+print(c)
+print(type(c))
+
+d=float(3)  #type casting
+print(d)
+print(type(d))
